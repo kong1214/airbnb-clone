cba9bfb57a3e33751db1cbf060fbdad15eb5c255
kong1214 <kevin.ong@Live.com>
2022-11-17 15:35:02 -0800

    Add User authentication Express middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,12 +5,14 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
+// router.get("/api/csrf/restore", (req, res) => {
+//     const csrfToken = req.csrfToken();
+//     res.cookie("XSRF-TOKEN", csrfToken);
+//     res.status(200).json({
+//       'XSRF-Token': csrfToken
+//     });
+//   });
+
+
 
 module.exports = router;
