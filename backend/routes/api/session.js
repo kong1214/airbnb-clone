32114da67328d48326f35d8cdce6de8eb32f9dbf
kong1214 <kevin.ong@Live.com>
2022-11-17 16:10:32 -0800

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -26,6 +26,15 @@ router.post(
         user: user
       });
     }
-  );
+);
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
 
 module.exports = router;
