fe4956a8f7539a03defff42f782813d5434b1dc6
kong1214 <kevin.ong@Live.com>
2022-11-23 16:19:57 -0800

    updated the res.json to {Spots: spots}

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
     })
     let reviewsObj = {}
     for (const review of reviews) {
-        const {spotId, stars} = review
+        const { spotId, stars } = review
         if (!reviewsObj[spotId]) {
             reviewsObj[spotId] = {};
             reviewsObj[spotId].stars = stars
@@ -37,7 +37,7 @@ router.get('/', async (req, res) => {
 
     const spots = await Spot.findAll({
         attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat',
-                    'lng', 'name', 'description', 'price', 'createdAt', 'updatedAt'],
+            'lng', 'name', 'description', 'price', 'createdAt', 'updatedAt'],
     })
 
     for (let spot of spots) {
@@ -53,8 +53,55 @@ router.get('/', async (req, res) => {
             }
         }
     }
-    
-    res.json(spots)
+
+    res.json({Spots: spots})
 })
 
 module.exports = router;
+
+//================================================================================================================//
+// const reviews = await Review.findAll({
+//     attributes: ['id', 'spotId', 'stars']
+// })
+// let reviewsObj = {}
+// for (const review of reviews) {
+//     const {spotId, stars} = review
+//     if (!reviewsObj[spotId]) {
+//         reviewsObj[spotId] = {};
+//         reviewsObj[spotId].stars = stars
+//         reviewsObj[spotId].numberOfReviews = 1
+//         reviewsObj[spotId].averageStars = stars
+//     }
+//     else {
+//         reviewsObj[spotId].numberOfReviews += 1
+//         reviewsObj[spotId].stars += stars
+//         reviewsObj[spotId].averageStars = reviewsObj[spotId].stars / reviewsObj[spotId].numberOfReviews
+//     }
+// }
+// const reviewsObjSpotIds = Object.keys(reviewsObj)
+
+// const previewImages = await SpotImage.findAll({
+//     where: {
+//         preview: true
+//     }
+// })
+
+// const spots = await Spot.findAll({
+//     attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat',
+//                 'lng', 'name', 'description', 'price', 'createdAt', 'updatedAt'],
+// })
+
+// for (let spot of spots) {
+//     for (let reviewsObjSpotId of reviewsObjSpotIds) {
+//         if (spot.id === Number(reviewsObjSpotId)) {
+//             let currentReview = reviewsObj[reviewsObjSpotId]
+//             spot.dataValues.avgRating = currentReview.averageStars
+//         }
+//     }
+//     for (let previewImage of previewImages) {
+//         if (!spot.dataValues.previewImage) {
+//             spot.dataValues.previewImage = previewImage.dataValues.url
+//         }
+//     }
+// }
+//================================================================================================================//
