6781019fae67498347db3019d49e3c5b110683b9
kong1214 <kevin.ong@Live.com>
2022-11-28 09:00:17 -0800

    took out many to many relationship through booking

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       Spot.hasMany(models.Booking, {foreignKey: 'spotId'})
       Spot.hasMany(models.Review, {foreignKey: 'spotId'})
       Spot.hasMany(models.SpotImage, {foreignKey: 'spotId'})
-      Spot.belongsToMany(models.User, { through: models.Booking })
+      // Spot.belongsToMany(models.User, { through: models.Booking })
     }
   }
   Spot.init({
