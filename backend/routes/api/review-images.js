cd8fd3671dce6b2193ee33a2a7d98c0ee05bbe97
kong1214 <kevin.ong@Live.com>
2022-11-28 00:14:03 -0800

    finished delete a review image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -8,7 +8,35 @@ const { Op } = require("sequelize")
 
 const router = express.Router();
 
+router.delete('/:reviewImageId', requireAuth, async (req, res, next) => {
+    const currentReviewImageId = Number(req.params.reviewImageId)
+    const loggedInUserId = res.req.user.dataValues.id
 
+    const reviewImageQueryTest = await ReviewImage.findOne({
+        where: {id: currentReviewImageId},
+        attributes: ["id", "reviewId"],
+        include: [{model: Review}]
+    })
+    // ERROR HANDLER if the spotImage doesn't exist
+    if (reviewImageQueryTest === null) {
+        const err = new Error()
+        err.message = "Review image couldn't be found";
+        err.status = 404;
+        err.statusCode = 404;
+        return next(err)
+    }
+    // ERROR HANDLER if the logged in user is not the owner of the spot
+    if (reviewImageQueryTest.dataValues.Review.dataValues.userId !== loggedInUserId)  {
+        const err = new Error()
+        err.message = "Review must belong to the current User"
+        err.status = 401
+        err.statusCode = 401
+        return next(err)
+    }
+
+    await reviewImageQueryTest.destroy()
+    res.json({message: "Successfully deleted"})
+})
 
 
 module.exports = router;
