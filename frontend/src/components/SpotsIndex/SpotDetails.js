536f449e471bbf690873ff1e8852ac281ffbac46
kong1214 <kevin.ong@Live.com>
2022-12-13 13:13:50 -0800

    added params

--- frontend/src/components/SpotsIndex/SpotDetails.js
+++ frontend/src/components/SpotsIndex/SpotDetails.js
@@ -1,5 +1,12 @@
-const SpotDetails = ({spotId}) => {
-    
+import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+const SpotDetails = ({}) => {
+    const { spotId } = useParams()
+
+    const spot = useSelector(state => state.spots)
+
+
+
     return (
         <a href={`/api/spots/${spot.id}`} className="card-container">
             <img src={`${spot.previewImage}`} />
