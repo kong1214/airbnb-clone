4e1035b754efd1da7d3635e513e0953beec86c6d
kong1214 <kevin.ong@Live.com>
2023-04-08 03:51:55 -0700

    finished

--- frontend/src/components/Bookings/CreateBookingModal.js
+++ frontend/src/components/Bookings/CreateBookingModal.js
@@ -15,15 +15,15 @@ function CreateBookingModal({ bookingInfo }) {
         return `${splitDate[1]}-${splitDate[2]}-${splitDate[0]}`
     }
     const handleConfirmation = async () => {
-        console.log(bookingInfo)
+        // console.log(bookingInfo)
         return dispatch(createBookingBySpot({
             startDate: bookingInfo.startDate,
             endDate: bookingInfo.endDate,
             spotId: bookingInfo.spotId
         }))
             .then(() => {
-                console.log(bookingInfo.startDate)
-                console.log(bookingInfo.endDate)
+                // console.log(bookingInfo.startDate)
+                // console.log(bookingInfo.endDate)
                 closeModal()
                 history.push("/bookings/current")
             })
